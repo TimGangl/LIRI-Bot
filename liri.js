ecba4ca8f9356ea03af6e5920c7bc805eb19f46d
Tim Gangl <tim.gangl30@gmail.com>
2019-11-23 10:39:57 -0800

    all packages installed, actions wrote

--- liri.js
+++ liri.js
@@ -1,3 +1,27 @@
 require("dotenv").config();
+// variables for requiring packages
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var axios = require("axios");
+var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
+var action = process.argv[2];
+var name = process.argv.splice(3).join();
 
 var keys = require("./keys.js");
+
+if (action === "concert-this") {
+  getConcertInformation();
+}
+else if (action === "spotify-this-song") {
+  getSongInfo();
+}
+else if (action === "do-what-it-says") {
+  getRandomInfo();
+}
+else if (action === "movie-this") {
+  getMovieInfo();
+}
+else {
+  console.log("Error: Invalid Entry")
+}
